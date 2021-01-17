cea1ae64ba5d2db2cc690e2364d4ac7cae1407ba
Tomasz Pałucki <tpalucki@gmail.com>
2021-01-17 17:54:41 +0100

    Question tip functionality updated.

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,85 +1,90 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {FormBuilder, Validators} from '@angular/forms';
-import {Quiz} from './quiz';
+import {Quiz} from './model/quiz';
+import {Question} from "./model/question";
 
 @Component({
-  selector: 'app-question',
-  templateUrl: './question.component.html',
-  styleUrls: ['./question.component.scss']
+    selector: 'app-question',
+    templateUrl: './question.component.html',
+    styleUrls: ['./question.component.scss']
 })
 export class QuestionComponent implements OnInit {
 
-  private quizUrl = "http://localhost:8080/api/v1/quiz/a";
+    quizForm;
+    tipVisible = false;
+    currentQuestion: Question;
+    answeredQuestions = new Array<any>();
+    quiz: Quiz;
+    showAnswer = false;
+    private quizUrl = "http://localhost:8080/api/v1/quiz/a";
 
-  quizForm;
-  tipVisible = false;
-  currentQuestion;
-  answeredQuestions = new Array<any>();
-  quiz: Quiz;
-  showAnswer = false;
-
-  constructor(private httpClient: HttpClient,
-              private formBuilder: FormBuilder) {
-    this.quizForm = this.formBuilder.group({
-      answerControl: [Validators.required]
-    });
-  }
-
-  ngOnInit() {
-    const headers = {};
-    this.httpClient
-    // .get<any>(this.quizUrl, {headers, observe: 'body', responseType: 'json'})
-      .get<any>("/assets/quiz.json")
-      .subscribe(data => {
-        this.quiz = data;
-        this.showNextQuestion();
-      })
-  }
-
-  showTip() {
-    this.tipVisible = true;
-  }
-
-  handleSubmitButtonClick(answerData: any) {
-    if (this.showAnswer) {
-      console.info('next questino');
-      this.showAnswer = false;
-      this.showNextQuestion();
-    } else {
-      console.info('show answer');
-      this.showAnswer = true;
-      this.answeredQuestions.push(this.currentQuestion);
-    }
-  }
-
-  nextQuestion() {
-    this.quizForm.reset();
-    this.tipVisible = false;
-
-    if (this.quiz.questions.length) {
-      this.showNextQuestion();
+    constructor(private httpClient: HttpClient,
+                private formBuilder: FormBuilder) {
+        this.quizForm = this.formBuilder.group({
+            answerControl: [Validators.required]
+        });
     }
 
-  }
+    ngOnInit() {
+        const headers = {};
+        this.httpClient
+        // .get<any>(this.quizUrl, {headers, observe: 'body', responseType: 'json'})
+            .get<any>("/assets/quiz.json")
+            .subscribe(data => {
+                this.quiz = data;
+                this.showNextQuestion();
+            })
+    }
 
-  private showNextQuestion() {
-    this.currentQuestion = this.quiz.questions.pop();
-  }
+    showTip() {
+        this.tipVisible = true;
+    }
 
-  isAnswerCorrect(): boolean {
-    return true;
-  }
+    hideTip() {
+        this.tipVisible = false;
+    }
 
-  alertType(): string {
-    return this.isAnswerCorrect() ? 'alert-success' : 'alert-danger';
-  }
+    handleSubmitButtonClick(answerData: any) {
+        if (this.showAnswer) {
+            console.info('next question');
+            this.showAnswer = false;
+            this.hideTip();
+            this.showNextQuestion();
+        } else {
+            console.info('show answer');
+            this.showAnswer = true;
+            this.answeredQuestions.push(this.currentQuestion);
+        }
+    }
 
-  alertMessage(): string {
-    return this.isAnswerCorrect() ? 'Well done!' : 'Wrong';
-  }
+    nextQuestion() {
+        this.quizForm.reset();
+        this.tipVisible = false;
 
-  submitButtonText() {
-    return this.showAnswer ? 'Next question' : 'Check';
-  }
+        if (this.quiz.questions.length) {
+            this.showNextQuestion();
+        }
+
+    }
+
+    isAnswerCorrect(): boolean {
+        return true;
+    }
+
+    alertType(): string {
+        return this.isAnswerCorrect() ? 'alert-success' : 'alert-danger';
+    }
+
+    alertMessage(): string {
+        return this.isAnswerCorrect() ? 'Well done!' : 'Wrong';
+    }
+
+    submitButtonText() {
+        return this.showAnswer ? 'Next question' : 'Check';
+    }
+
+    private showNextQuestion() {
+        this.currentQuestion = this.quiz.questions.pop();
+    }
 }
